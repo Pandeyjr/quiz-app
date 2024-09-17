b972539b2c0edcca60a1fb0fa3724ab386d89122
Pandeyjr17 <kp842586@gmail.com>
2024-09-17 13:08:37 +0545

    Updated quiz detail and list templates, modified views

--- quiz/views.py
+++ quiz/views.py
@@ -10,18 +10,26 @@ def quiz_list(request):
     quizzes = Quiz.objects.all()
     return render(request, 'quiz/quiz_list.html', {'quizzes': quizzes})
 
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
+from .models import Quiz, Answer
 
 def quiz_detail(request, quiz_id):
     quiz = get_object_or_404(Quiz, pk=quiz_id)
 
     if request.method == 'POST':
         score = 0
+        total_questions = quiz.questions.count()
+
         for question in quiz.questions.all():
             selected_answer_id = request.POST.get(f'question_{question.id}')
             if selected_answer_id:
                 selected_answer = Answer.objects.get(id=selected_answer_id)
                 if selected_answer.is_correct:
                     score += 1
-        return HttpResponse(f'Your score: {score} / {quiz.questions.count()}')
+
+        # Return score as JSON response
+        return JsonResponse({'score': score, 'total': total_questions})
 
     return render(request, 'quiz/quiz_detail.html', {'quiz': quiz})
+
